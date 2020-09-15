fbc7e0b27d015f1731b58aa7474754124a561109
Mathertone <max.ather10@gmail.com>
2020-09-15 10:29:05 -0500

    Add files via upload

---
+++ static/js/coxcomb.js
@@ -0,0 +1,684 @@
+var data1 = [{
+    "id": 0,
+    "key": "Causes",
+    "year": 1999,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 98760
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 44536
+    }, {
+        "key": "Stroke",
+        "value": 167366
+    }, {
+        "key": "CLRD",
+        "value": 124181
+    }, {
+        "key": "Diabetes",
+        "value": 68399
+    }, {
+        "key": "Heart disease",
+        "value": 725192
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 63730
+    }, {
+        "key": "Suicide",
+        "value": 29199
+    }, {
+        "key": "Cancer",
+        "value": 549838
+    }, {
+        "key": "Kidney disease",
+        "value": 35525                          
+    }]
+},
+{
+    "id": 1,
+    "key": "Causes",
+    "year": 2000,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 97900
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 49558
+    }, {
+        "key": "Stroke",
+        "value": 167661
+    }, {
+        "key": "CLRD",
+        "value": 122009
+    }, {
+        "key": "Diabetes",
+        "value": 69301
+    }, {
+        "key": "Heart disease",
+        "value": 710760
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 65313
+    }, {
+        "key": "Suicide",
+        "value": 29350
+    }, {
+        "key": "Cancer",
+        "value": 553091
+    }, {
+        "key": "Kidney disease",
+        "value": 37251                          
+    }]
+},
+{
+    "id": 2,
+    "key": "Causes",
+    "year": 2001,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 101537
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 53852
+    }, {
+        "key": "Stroke",
+        "value": 163538
+    }, {
+        "key": "CLRD",
+        "value": 123013
+    }, {
+        "key": "Diabetes",
+        "value": 71372
+    }, {
+        "key": "Heart disease",
+        "value": 700142
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 62034
+    }, {
+        "key": "Suicide",
+        "value": 30622
+    }, {
+        "key": "Cancer",
+        "value": 553768
+    }, {
+        "key": "Kidney disease",
+        "value": 39480                          
+    }]
+},
+{
+    "id": 3,
+    "key": "Causes",
+    "year": 2002,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 106742
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 58866
+    }, {
+        "key": "Stroke",
+        "value": 162672
+    }, {
+        "key": "CLRD",
+        "value": 124816
+    }, {
+        "key": "Diabetes",
+        "value": 73249
+    }, {
+        "key": "Heart disease",
+        "value": 696947
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 65681
+    }, {
+        "key": "Suicide",
+        "value": 31655
+    }, {
+        "key": "Cancer",
+        "value": 557271
+    }, {
+        "key": "Kidney disease",
+        "value": 40974                          
+    }]
+},
+{
+    "id": 4,
+    "key": "Causes",
+    "year": 2003,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 109277
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 63457
+    }, {
+        "key": "Stroke",
+        "value": 157689
+    }, {
+        "key": "CLRD",
+        "value": 126382
+    }, {
+        "key": "Diabetes",
+        "value": 74219
+    }, {
+        "key": "Heart disease",
+        "value": 685089
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 65163
+    }, {
+        "key": "Suicide",
+        "value": 31484
+    }, {
+        "key": "Cancer",
+        "value": 556902
+    }, {
+        "key": "Kidney disease",
+        "value": 42453                          
+    }]
+},
+{
+    "id": 5,
+    "key": "Causes",
+    "year": 2004,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 112012
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 65965
+    }, {
+        "key": "Stroke",
+        "value": 150074
+    }, {
+        "key": "CLRD",
+        "value": 121987
+    }, {
+        "key": "Diabetes",
+        "value": 73138
+    }, {
+        "key": "Heart disease",
+        "value": 652486
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 59664
+    }, {
+        "key": "Suicide",
+        "value": 32439
+    }, {
+        "key": "Cancer",
+        "value": 553888
+    }, {
+        "key": "Kidney disease",
+        "value": 42480                          
+    }]
+},
+{
+    "id": 6,
+    "key": "Causes",
+    "year": 2005,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 117809
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 71599
+    }, {
+        "key": "Stroke",
+        "value": 143579
+    }, {
+        "key": "CLRD",
+        "value": 130933
+    }, {
+        "key": "Diabetes",
+        "value": 75119
+    }, {
+        "key": "Heart disease",
+        "value": 652091
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 63001
+    }, {
+        "key": "Suicide",
+        "value": 32637
+    }, {
+        "key": "Cancer",
+        "value": 559312
+    }, {
+        "key": "Kidney disease",
+        "value": 43901                          
+    }]
+},
+{
+    "id": 7,
+    "key": "Causes",
+    "year": 2006,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 121599
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 72432
+    }, {
+        "key": "Stroke",
+        "value": 137119
+    }, {
+        "key": "CLRD",
+        "value": 124583
+    }, {
+        "key": "Diabetes",
+        "value": 72449
+    }, {
+        "key": "Heart disease",
+        "value": 631636
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 56326
+    }, {
+        "key": "Suicide",
+        "value": 33300
+    }, {
+        "key": "Cancer",
+        "value": 559888
+    }, {
+        "key": "Kidney disease",
+        "value": 45344                          
+    }]
+},
+{
+    "id": 8,
+    "key": "Causes",
+    "year": 2007,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 123706
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 74632
+    }, {
+        "key": "Stroke",
+        "value": 135952
+    }, {
+        "key": "CLRD",
+        "value": 127924
+    }, {
+        "key": "Diabetes",
+        "value": 71382
+    }, {
+        "key": "Heart disease",
+        "value": 616067
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 52717
+    }, {
+        "key": "Suicide",
+        "value": 34598
+    }, {
+        "key": "Cancer",
+        "value": 562875
+    }, {
+        "key": "Kidney disease",
+        "value": 46448                          
+    }]
+},
+{
+    "id": 9,
+    "key": "Causes",
+    "year": 2008,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 121902
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 82435
+    }, {
+        "key": "Stroke",
+        "value": 134148
+    }, {
+        "key": "CLRD",
+        "value": 141090
+    }, {
+        "key": "Diabetes",
+        "value": 70553
+    }, {
+        "key": "Heart disease",
+        "value": 616828
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 56284
+    }, {
+        "key": "Suicide",
+        "value": 36035
+    }, {
+        "key": "Cancer",
+        "value": 565469
+    }, {
+        "key": "Kidney disease",
+        "value": 48237                          
+    }]
+},
+{
+    "id": 10,
+    "key": "Causes",
+    "year": 2009,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 118021
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 79003
+    }, {
+        "key": "Stroke",
+        "value": 128842
+    }, {
+        "key": "CLRD",
+        "value": 137353
+    }, {
+        "key": "Diabetes",
+        "value": 68705
+    }, {
+        "key": "Heart disease",
+        "value": 599413
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 53692
+    }, {
+        "key": "Suicide",
+        "value": 36909
+    }, {
+        "key": "Cancer",
+        "value": 567628
+    }, {
+        "key": "Kidney disease",
+        "value": 48935                          
+    }]
+},
+{
+    "id": 11,
+    "key": "Causes",
+    "year": 2010,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 120859
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 83494
+    }, {
+        "key": "Stroke",
+        "value": 129476
+    }, {
+        "key": "CLRD",
+        "value": 138080
+    }, {
+        "key": "Diabetes",
+        "value": 69071
+    }, {
+        "key": "Heart disease",
+        "value": 597689
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 50097
+    }, {
+        "key": "Suicide",
+        "value": 38364
+    }, {
+        "key": "Cancer",
+        "value": 574743
+    }, {
+        "key": "Kidney disease",
+        "value": 50476                          
+    }]
+},
+{
+    "id": 12,
+    "key": "Causes",
+    "year": 2011,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 126438
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 84974
+    }, {
+        "key": "Stroke",
+        "value": 128932
+    }, {
+        "key": "CLRD",
+        "value": 142943
+    }, {
+        "key": "Diabetes",
+        "value": 73831
+    }, {
+        "key": "Heart disease",
+        "value": 596577
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 53826
+    }, {
+        "key": "Suicide",
+        "value": 39518
+    }, {
+        "key": "Cancer",
+        "value": 576691
+    }, {
+        "key": "Kidney disease",
+        "value": 45591                          
+    }]
+},
+{
+    "id": 13,
+    "key": "Causes",
+    "year": 2012,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 127792
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 83637
+    }, {
+        "key": "Stroke",
+        "value": 128546
+    }, {
+        "key": "CLRD",
+        "value": 143489
+    }, {
+        "key": "Diabetes",
+        "value": 73932
+    }, {
+        "key": "Heart disease",
+        "value": 599711
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 50636
+    }, {
+        "key": "Suicide",
+        "value": 40600
+    }, {
+        "key": "Cancer",
+        "value": 582623
+    }, {
+        "key": "Kidney disease",
+        "value": 45591                          
+    }]
+},
+{
+    "id": 14,
+    "key": "Causes",
+    "year": 2013,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 130557
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 84767
+    }, {
+        "key": "Stroke",
+        "value": 128978
+    }, {
+        "key": "CLRD",
+        "value": 149205
+    }, {
+        "key": "Diabetes",
+        "value": 75578
+    }, {
+        "key": "Heart disease",
+        "value": 611105
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 56979
+    }, {
+        "key": "Suicide",
+        "value": 41149
+    }, {
+        "key": "Cancer",
+        "value": 584881
+    }, {
+        "key": "Kidney disease",
+        "value": 47112                          
+    }]
+},
+{
+    "id": 15,
+    "key": "Causes",
+    "year": 2014,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 135928
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 93541
+    }, {
+        "key": "Stroke",
+        "value": 133103
+    }, {
+        "key": "CLRD",
+        "value": 147101
+    }, {
+        "key": "Diabetes",
+        "value": 76488
+    }, {
+        "key": "Heart disease",
+        "value": 614348
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 55227
+    }, {
+        "key": "Suicide",
+        "value": 42826
+    }, {
+        "key": "Cancer",
+        "value": 591700
+    }, {
+        "key": "Kidney disease",
+        "value": 48146                          
+    }]
+},
+{
+    "id": 16,
+    "key": "Causes",
+    "year": 2015,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 146571
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 110561
+    }, {
+        "key": "Stroke",
+        "value": 140323
+    }, {
+        "key": "CLRD",
+        "value": 155041
+    }, {
+        "key": "Diabetes",
+        "value": 79535
+    }, {
+        "key": "Heart disease",
+        "value": 633842
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 57062
+    }, {
+        "key": "Suicide",
+        "value": 44193
+    }, {
+        "key": "Cancer",
+        "value": 595930
+    }, {
+        "key": "Kidney disease",
+        "value": 49959                          
+    }]
+},
+{
+    "id": 17,
+    "key": "Causes",
+    "year": 2016,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 161374
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 116103
+    }, {
+        "key": "Stroke",
+        "value": 142142
+    }, {
+        "key": "CLRD",
+        "value": 154596
+    }, {
+        "key": "Diabetes",
+        "value": 80058
+    }, {
+        "key": "Heart disease",
+        "value": 635260
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 51537
+    }, {
+        "key": "Suicide",
+        "value": 44965
+    }, {
+        "key": "Cancer",
+        "value": 598038
+    }, {
+        "key": "Kidney disease",
+        "value": 50046                          
+    }]
+},
+{
+    "id": 18,
+    "key": "Causes",
+    "year": 2017,
+    "values": [{
+        "key": "Unintentional injuries",
+        "value": 169936
+    }, {
+        "key": "Alzheimer's disease",
+        "value": 121404
+    }, {
+        "key": "Stroke",
+        "value": 146383
+    }, {
+        "key": "CLRD",
+        "value": 160201
+    }, {
+        "key": "Diabetes",
+        "value": 83564
+    }, {
+        "key": "Heart disease",
+        "value": 647457
+    }, {
+        "key": "Influenza, pneumonia",
+        "value": 55672
+    }, {
+        "key": "Suicide",
+        "value": 47173
+    }, {
+        "key": "Cancer",
+        "value": 599108
+    }, {
+        "key": "Kidney disease",
+        "value": 50633                          
+    }]           
+}];
